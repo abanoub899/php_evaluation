dbdc791274e0f30f15792b0cf0c7547d158f42c6
abanoub899 <abanobgayed@outlook.com>
2021-08-22 01:31:58 -0700

    abanob

---
+++ login.php
@@ -0,0 +1,64 @@
+<?php
+session_start();
+// header.php
+include ('header.php');
+include "helper.php";
+?>
+
+
+<?php
+include("config.php");
+// session_start();
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+   // username and password sent from form 
+   
+   $myusername = mysqli_real_escape_string($db,$_POST['username']);
+   $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+   
+   $sql = "SELECT id FROM users WHERE username = '$myusername' and password = '$mypassword'";
+   $result = mysqli_query($db,$sql);
+   $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+   $active = $row['active'];
+   
+   $count = mysqli_num_rows($result);
+   
+   // If result matched $myusername and $mypassword, table row must be 1 row
+ 
+   if($count == 1) {
+      // session_register("myusername");
+      $_SESSION['login_user'] = $myusername;
+      // echo "here comes login user";
+      header("location: welcome.php");
+   }else {
+      $error = "Your Login Name or Password is invalid";
+   }
+}
+?>
+
+<section id="login-form">
+<div class="container">
+  <h2>login page</h2>
+  <form action = "" method = "post">
+    <div class="form-group">
+      <label for="username">UserName:</label>
+      <input type="text" class="form-control" id="username" placeholder="Enter UserName" name="username"require>
+    </div>
+    <div class="form-group">
+      <label for="password">Password:</label>
+      <input type="password" class="form-control" id="password" placeholder="Enter password" name="password"require>
+    </div>
+    <div class="form-group form-check">
+      <label class="form-check-label">
+        <input class="form-check-input" type="checkbox" name="remember"> Remember password
+      </label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</div>
+</section>
+
+<?php
+// footer.php
+include ('footer.php');
+?>
